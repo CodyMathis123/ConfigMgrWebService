c5e74a7457da3c45cdb39c0ccad47a97fe7c008f
CodyMathis123 <cody.mathis123@gmail.com>
2017-06-13 13:56:08 -0400

    ADUser class

---
+++ ConfigMgrWebService/ADUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConfigMgrWebService
+{
+    public class ADUser
+    {
+        public ADUser(string distinguishedName, string displayName, string userName, string email)
+        {
+            this.distinguishedName = distinguishedName;
+            this.displayName = displayName;
+            this.userName = userName;
+            this.email = email;
+        }
+        public string distinguishedName { set; get; }
+        public string displayName { set; get; }
+        public string userName { set; get; }
+        public string email { set; get; }
+
+        private ADUser() { }
+    }
+}
